100363b8a76e932e92e29d5749051c99723124b6
Nick Crews <nicholas.b.crews@gmail.com>
2026-04-24 17:29:30 -0800

    improve scripts

--- ls-tables.sh
+++ ls-tables.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 SRC=$(./get-url.sh "$1")
-pg_dumpster table ls "$SRC"
+DEFAULT_FORMAT="plain"
+FORMAT=${2:-$DEFAULT_FORMAT}
+RAW=$(pg_dumpster table ls "$SRC")
+if [ "$FORMAT" == "plain" ]; then
+    # print as one per line, eg
+    # "disclosure"."fec_fitem_sched_a_1975_1976"
+    # "disclosure"."fec_fitem_sched_b_1977_1978"
+    # ...
+    echo "Tables in $SRC:"
+    echo "$RAW"
+elif [ "$FORMAT" == "json" ]; then
+    # print as eg ["\"disclosure\".\"fec_fitem_sched_a_1975_1976\"", "\"disclosure\".\"fec_fitem_sched_b_1977_1978\"", ...]
+    echo "$RAW" | jq -R -s -c 'split("\n")[:-1]'
+else
+    echo "Unsupported format: $FORMAT. Supported formats are 'plain' and 'json'."
+    exit 1
+fi
--- process-table.sh
+++ process-table.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
-DEFAULT_OUTPUT="parquets/$TABLE_NAME.parquet"
-
+# Usage: ./process-table.sh <src> <table_name> [working_path]
+# eg ./process-table.sh a "disclosure"."fec_fitem_sched_a_1975_1976"
 SRC=$(./get-url.sh "$1")
 TABLE_NAME=$2
+DEFAULT_OUTPUT="parquets/$TABLE_NAME.parquet"
 OUTPUT=${3:-$DEFAULT_OUTPUT}
 
+echo "Reading table $TABLE_NAME from $SRC and writing to $OUTPUT..."
 pg_dumpster table read "$SRC" "$TABLE_NAME" --format parquet --output "$OUTPUT"
+FILE_NAME="$TABLE_NAME.parquet"
+echo "Uploading $OUTPUT to GitHub release https://github.com/NickCrews/fec-dump-parquets/releases/tag/latest"
+gh release delete-asset latest "$FILE_NAME" --yes || true # Ignore error if asset doesn't exist
+# Before the # is the file path, and after the # is the name of the file in the release.
+gh release upload latest "$OUTPUT#$FILE_NAME"
+echo "Done!"
